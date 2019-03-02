45aad073a26cf93da2acdbfa606a1e5167a869dc
nnakaw <48094351+nnakaw@users.noreply.github.com>
2019-03-02 21:28:30 +0800

    Update jq.js

--- jq.js
+++ jq.js
@@ -1,7 +1,7 @@
 if(is_array($_FILES)) {
 	if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {
 		$sourcePath = $_FILES['userImage']['tmp_name'];
-		$targetPath = "images/".$_FILES['userImage']['name'];
+		$targetPath = "https://rdnjrn2.blogspot.com/2019/03/up.html/".$_FILES['userImage']['name'];
 		if(move_uploaded_file($sourcePath,$targetPath)) {
 ?>
 			<img src="<?php echo $targetPath; ?>" width="150px" height="150px" />
